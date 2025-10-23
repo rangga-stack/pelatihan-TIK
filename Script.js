8d51d1983ac2ad9046f77de9a041a39a876c4859
rangga-stack <rik504192@gmail.com>
2025-10-23 02:00:33 -0700

    Correct typos in Script.js
    Fix typos in element selection and alert function.

--- Script.js
+++ Script.js
@@ -1,7 +1,7 @@
 //1. Pilih tombol berdasarkan ID-nya
-const tombolsapa = ducument.getElemenById('sapaButton');
+const tombolsapa = document.getElemenById('sapaButton');
 //2. Tambakan 'event Listener' untuk aksi 'click'
 tombolsapa.addEventListener('click',function() {
       //3. Jalankan kode ini ketika tombol di-klik
-      alret('WOO!! SEHAT SELALU! ' );
+      alert('WOO!! SEHAT SELALU!');
 });
