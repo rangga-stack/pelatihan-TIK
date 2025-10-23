1190fa90c83e12a77e1a48696a3b527f4d9ed72b
rangga-stack <rik504192@gmail.com>
2025-10-22 20:17:47 -0700

    Add button click event listener in Script.js

---
+++ Script.js
@@ -0,0 +1,7 @@
+//1. Pilih tombol berdasarkan ID-nya
+const tombolsapa = ducument.getElemenById('sapaButton');
+//2. Tambakan 'event Listener' untuk aksi 'click'
+tombolsapa.addEventListener('click',function() {
+      //3. Jalankan kode ini ketika tombol di-klik
+      alret('WOO!! SEHAT SELALU! ' );
+});
